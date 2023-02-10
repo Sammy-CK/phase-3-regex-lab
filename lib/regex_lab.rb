aca592ad805ad58c05fcecb97169f5df2431bb6c
Sammy-CK <sammycherono2696@gmail.com>
2023-02-10 11:00:46 +0300

    Completed assignment

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,26 +1,23 @@
 def starts_with_a_vowel?(word)
-    !!word.match(/^[aeiou\W]/i)
+  !!word.match(/^[aeiou\W]/i)
 end
 
 starts_with_a_vowel?("ass")
 
 def words_starting_with_un_and_ending_with_ing(text)
-    text.scan(/un*+\w*ing/)
+  text.scan(/un*+\w*ing/)
 end
 
 def words_five_letters_long(text)
-   pp text.scan(/\b\w{5}\b/)
-
+  pp text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-    text.match(/^[A-Z].*[a-zA-Z][.]$/) ? true : false
+  text.match(/^[A-Z].*[a-zA-Z][.]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-   pp !!phone.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)
+  pp !!phone.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)
 end
 
-
-    valid_phone_number?("212334sss")
-
+valid_phone_number?("212334sss")
